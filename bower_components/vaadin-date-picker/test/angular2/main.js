04584b27074e594cc3091a1a78eaaf61292e3ad5
Wolfgang Dietersdorfer <Wolfgang Dietersdorfer>
2016-11-05 22:23:29 +0100

    bower and node modules PLUS fix for tsconfig.json

---
+++ bower_components/vaadin-date-picker/test/angular2/main.js
@@ -0,0 +1,9 @@
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var core_1 = require('@angular/core');
+var app_module_1 = require('./app.module');
+core_1.enableProdMode();
+document.body.addEventListener('bootstrap', function () {
+    platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
+});
+//# sourceMappingURL=main.js.map
